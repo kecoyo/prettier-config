d7ae6f5d03c020dbb8c73fb95803e4b06d5d87b6
杨可可 <kecoyo@163.com>
2023-02-22 10:23:34 +0800

    1.0.0

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 module.exports = {
-  printWidth: 150,
-  singleQuote: true,
-  trailingComma: 'es5',
-  semi: true,
+  arrowParens: "avoid", // 单参数箭头函数参数周围不使用圆括号
+  printWidth: 200, // 单行长度，超过自动换行
+  singleQuote: true, // 使用单引号
+  tabWidth: 2, // 缩进长度
+  trailingComma: "es5", // 允许函数的最后一个参数有尾逗号
+  semi: true, // 句末使用分号
 };

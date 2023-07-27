55ab7f14223229820be19c066ac775b3f59a30a7
杨可可 <yangkeke@dmtop1.com>
2023-07-27 17:30:30 +0800

    1.0.1

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 module.exports = {
-  arrowParens: "avoid", // 单参数箭头函数参数周围不使用圆括号
   printWidth: 200, // 单行长度，超过自动换行
+  arrowParens: "avoid", // 单参数箭头函数参数周围不使用圆括号
   singleQuote: true, // 使用单引号
   tabWidth: 2, // 缩进长度
-  trailingComma: "es5", // 允许函数的最后一个参数有尾逗号
+  trailingComma: "all", // 允许函数的最后一个参数有尾逗号
   semi: true, // 句末使用分号
+  proseWrap: "never",
+  overrides: [{ files: ".prettierrc", options: { parser: "json" } }],
+  plugins: ["prettier-plugin-organize-imports", "prettier-plugin-packagejson"],
 };

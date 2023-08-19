c0abbd10fcbf9cae89b2c90e948543b623b7a12c
杨可可 <yangkeke@dmtop1.com>
2023-08-20 07:22:44 +0800

    1.0.2

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 module.exports = {
   printWidth: 200, // 单行长度，超过自动换行
-  arrowParens: "avoid", // 单参数箭头函数参数周围不使用圆括号
+  arrowParens: "always", // 单参数箭头函数参数周围不使用圆括号
   singleQuote: true, // 使用单引号
+  useTabs: false,
   tabWidth: 2, // 缩进长度
+  endOfLine: "lf",
   trailingComma: "all", // 允许函数的最后一个参数有尾逗号
   semi: true, // 句末使用分号
   proseWrap: "never",
